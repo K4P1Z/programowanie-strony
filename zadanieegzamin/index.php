620b5fe879d430b2936eed82d0a728bbbf271a1d
Kacper <144652457+K4P1Z@users.noreply.github.com>
2024-05-21 10:15:56 +0200

    Add files via upload

---
+++ zadanieegzamin/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+    <div class="left">
+    <ul>
+        <h2>Menu</h2>
+        <li><a href="#">Kalkulator</a></li>
+        <li><a href="#">Samochody</a></li>
+        <li><a href="#">Kontakt</a></li>
+    </ul>
+    </div>
+<div class="right">
+    <div class="header">
+        <header>Aplikacja webowa</header>
+    </div>
+
+    <div class="contener">
+        
+    <div class="obliczanie">
+    <div class="wiersz1"><p>Spalanie na 100km</p><input type="text"></div>
+    <div class="wiersz2"><p>Przejechany dystans</p><input type="text"></div>
+    <div class="wiersz3"><p>Cena paliwa</p><input type="text"></div>
+    </div>
+    <button>Oblicz</button>
+    </div>
+    <footer>
+        Wszelkie prawa zastrzeżone
+    </footer>
+    </div>
+    </main>
+</body>
+</html>
